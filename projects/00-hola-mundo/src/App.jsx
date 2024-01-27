0db2c8c3f7eafeea6659571295183fe179078782
tolerevo <tolerevo@hotmail.com>
2024-01-27 16:45:58 -0600

    mend

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,7 +1,19 @@
 export const App = () => {
   return (
-    <div>
-      <h1>Twitter Card</h1>
-    </div>
+    <article>
+      <header>
+        <img src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" alt="avatar de Tom" />
+        <div>
+          <strong>Tom the Cat</strong>
+          <span>@Tomthecat</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
   );
 };
