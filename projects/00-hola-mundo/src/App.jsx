3beee6402a4c0b84d7609657be72abbb4e92bc48
tolerevo <tolerevo@hotmail.com>
2024-02-03 13:05:01 -0600

    ammend
    Signed-off-by: tolerevo <tolerevo@hotmail.com>

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,19 +1,8 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
 export const App = () => {
   return (
-    <article>
-      <header>
-        <img src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" alt="avatar de Tom" />
-        <div>
-          <strong>Tom the Cat</strong>
-          <span>@Tomthecat</span>
-        </div>
-      </header>
-
-      <aside>
-        <button>
-          Seguir
-        </button>
-      </aside>
-    </article>
+  <TwitterFollowCard userName="tolerevo" name="Diego Tolentino" />
   );
 };
---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,24 @@
+export function TwitterFollowCard({ userName, name, isFollowing }) {
+  return (
+    <article
+      className="tw-followCard"
+      style={{ display: "flex", alignItems: "center", color: "#fff" }}
+    >
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src={`https://unavatar.io/${userName}`}
+          alt="avatar de Tolerevo"
+        />
+        <div className="tw-followCard-info">
+          <strong>{name}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>
+    </article>
+  );
+}
